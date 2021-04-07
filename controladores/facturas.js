cb67f9103a9d20c54d313b55d45d466fd9c3fdad
Fede Montagud <fedemontagudo@gmail.com>
2021-04-07 17:02:43 +0200

    Endpoint get by id devuelve de momento error 404 el endpoint no existe

--- controladores/facturas.js
+++ controladores/facturas.js
@@ -1,3 +1,4 @@
+const { generaError } = require("../utils/errores");
 const facturasJSON = require("../facturas.json").facturas;
 
 const getFacturas = () => ({
@@ -15,8 +16,24 @@ const getGastos = () => ({
   datos: facturasJSON.filter(factura => factura.tipo === "gasto")
 });
 
+const getFactura = id => {
+  const factura = facturasJSON.find(factura => factura.id === id);
+  const respuesta = {
+    factura: null,
+    error: null
+  };
+  if (factura) {
+    respuesta.factura = factura;
+  } else {
+    const error = generaError("La factura solicitada no existe", 404);
+    respuesta.error = error;
+  }
+  return respuesta;
+};
+
 module.exports = {
   getFacturas,
   getIngresos,
-  getGastos
+  getGastos,
+  getFactura
 };
--- rutas/facturas.js
+++ rutas/facturas.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getFacturas, getIngresos, getGastos } = require("../controladores/facturas");
+const {
+  getFacturas, getIngresos, getGastos, getFactura
+} = require("../controladores/facturas");
 
 router.get("/", (req, res, next) => {
   const listaFacturas = getFacturas();
@@ -17,6 +19,16 @@ router.get("/ingresos", (req, res, next) => {
 router.get("/gastos", (req, res, next) => {
   const gastos = getGastos();
   res.json(gastos);
+});
+
+router.get("factura/:idFactura", (req, res, next) => {
+  const idFactura = +req.params.id;
+  const { factura, error } = getFactura(idFactura);
+  if (error) {
+    next(error);
+  } else {
+    res.json(factura);
+  }
 });
 
 module.exports = router;
